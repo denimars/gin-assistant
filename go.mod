5913778b636a27655de686b0bf237804206fe5a1
denimarswnadi <denimarswandi@gmail.com>
2025-02-07 07:41:35 +0700

    watcher file

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/cors v1.7.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"gin-assistant/command"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 /**
@@ -18,6 +24,36 @@ import (
 *|--run
 *|main
 **/
+
+var cmd *exec.Cmd
+
+func run() {
+	if cmd != nil {
+		_ = cmd.Process.Kill()
+	}
+	cmd = exec.Command("go", "run", "main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+	fmt.Println("Server restarted at", time.Now())
+}
+
+func watchFiles(watcher *fsnotify.Watcher, dir string) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			fmt.Println("👀 Watching:", path)
+			return watcher.Add(path)
+		}
+		return nil
+	})
+}
 
 func main() {
 	flag.Parse()
@@ -36,6 +72,38 @@ func main() {
 			}
 		case "auth":
 			command.Auth(dir)
+		case "run":
+			watcher, err := fsnotify.NewWatcher()
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer watcher.Close()
+
+			projectDir := "."
+			err = watchFiles(watcher, projectDir)
+			if err != nil {
+				log.Fatal(err)
+			}
+			run()
+			fmt.Println("🚀 Watching for file changes...")
+
+			for {
+				select {
+				case event, ok := <-watcher.Events:
+					if !ok {
+						return
+					}
+					if event.Op == fsnotify.Write || event.Op == fsnotify.Create || event.Op == fsnotify.Remove {
+						fmt.Println("🔄 File changed:", event.Name)
+						run()
+					}
+				case err, ok := <-watcher.Errors:
+					if !ok {
+						return
+					}
+					fmt.Println("❌ Watcher error:", err)
+				}
+			}
 		default:
 			fmt.Println("command not found")
 		}
